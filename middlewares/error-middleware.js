7d987c167352e0c9706b21f9061a4ab3797e1ac2
Mike Tase <tasemgt@gmail.com>
2020-12-12 10:48:04 +0100

    eror handling bug fix

--- middlewares/error-middleware.js
+++ middlewares/error-middleware.js
@@ -87,21 +87,22 @@ module.exports = (err, req, res, next) =>{
   err.status = err.status || 'error';
 
   console.log(err);
+  sendErrorDev(err, req, res);
 
-  if(process.env.NODE_ENV === 'development'){
-    console.log('In development.....')
-    sendErrorDev(err, req, res);
-  } 
-  else if(process.env.NODE_ENV === 'production'){
+  // if(process.env.NODE_ENV === 'development'){
+  //   console.log('In development.....')
+  //   sendErrorDev(err, req, res);
+  // } 
+  // else if(process.env.NODE_ENV === 'production'){
 
-    let error = {...err};
+  //   let error = {...err};
     
-    if(error.name === 'CastError'){
-      error = handleCastErrorDB(error);
-    }
-    else{
-      sendErrorDev(error, req, res);
-    }
-  }
+  //   if(error.name === 'CastError'){
+  //     error = handleCastErrorDB(error);
+  //   }
+  //   else{
+  //     sendErrorDev(error, req, res);
+  //   }
+  // }
   
 };
