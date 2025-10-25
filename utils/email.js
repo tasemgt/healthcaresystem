f5803357c227e07c03e33dc1304a8c10b2c2fd96
Michael Tase <tasemgt@gmail.com>
2025-10-25 17:02:54 +0100

    fix bug

--- utils/email.js
+++ utils/email.js
@@ -31,8 +31,8 @@ const sendProd = async options =>{
     from: process.env.FROM_EMAIL,
     to: options.email,
     subject: options.subject,
-    text: options.text,
-    html: options.html || `<p>${text}</p>`,
+    text: options.message,
+    html: options.html || `<p>${options.message}</p>`,
   };
 
   try {
